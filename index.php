51cfbccfb1da4596d37f19e0ed0a7ccf91a05a68
Tim Chabra <68378408+TimChabra@users.noreply.github.com>
2020-11-29 07:25:12 -0500

    Update and rename index.php to index.rb

--- index.php
+++
@@ -1,3 +0,0 @@
-Hi from <?php 
-print("ITOP");
-?>
---
+++ index.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+system("clear")
+
+puts ""
+puts ">=================> PROGRESS >==================>";
+puts ""
+
+# Run
+puts "OK - Hi from ITOP"
